32566a539aac8012c073023c2bb57746ffabc390
蔡序强 <1781088743@qq.com>
2020-11-30 16:21:26 +0800

    启动类加图案

--- src/main/java/cn/caixiaobai/springboot/SpringbootApplication.java
+++ src/main/java/cn/caixiaobai/springboot/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package cn.caixiaobai.springboot;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +13,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("cn.caixiaobai.springboot.mapper")
+@Slf4j
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class,args);
+        System.out.println("  .   ____          _            __ _ _\n" +
+                " /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\\n" +
+                "( ( )\\___ | '_ | '_| | '_ \\/ _` | \\ \\ \\ \\\n" +
+                " \\\\/  ___)| |_)| | | | | || (_| |  ) ) ) )\n" +
+                "  '  |____| .__|_| |_|_| |_\\__, | / / / /\n" +
+                " =========|_|==============|___/=/_/_/_/\n" +
+                " :: Spring Boot ::        (v2.3.4.RELEASE)");
+        log.info("恭喜！项目启动成功！");
     }
 }
