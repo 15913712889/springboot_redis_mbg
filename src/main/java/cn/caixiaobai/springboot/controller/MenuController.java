31b1f2c398a87fd6d1764e230bece1baa6d09fbc
蔡序强 <1781088743@qq.com>
2020-11-30 09:44:13 +0800

    提交未提交的controllelr

---
+++ src/main/java/cn/caixiaobai/springboot/controller/MenuController.java
@@ -0,0 +1,20 @@
+package cn.caixiaobai.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 菜单表 前端控制器
+ * </p>
+ *
+ * @author caixiaobai
+ * @since 2020-11-24
+ */
+@RestController
+@RequestMapping("/springboot/menu")
+public class MenuController {
+
+}
