a912217214f0907eaabfc452a19ccb9741ac1bcf
蔡序强 <1781088743@qq.com>
2020-12-01 10:31:57 +0800

    reids操作hash的相关方法

--- src/main/java/cn/caixiaobai/springboot/controller/redis/OperationRedisHashController.java
+++ src/main/java/cn/caixiaobai/springboot/controller/redis/OperationRedisHashController.java
@@ -4,18 +4,13 @@ import cn.caixiaobai.springboot.dto.User;
 import cn.caixiaobai.springboot.result.SystemResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @Author: caixiaobai
@@ -124,9 +119,107 @@ public class OperationRedisHashController {
     public SystemResult demo08(){
         //获取hash操作对象
         HashOperations hashOperations = redisTemplate.opsForHash();
+        //返回hash值为newRedis_cxq,key为user所对应value的长度
+        Long aLong = hashOperations.lengthOfValue("newRedis_cxq", "user");
 
+        return SystemResult.ok();
+    }
 
+    @RequestMapping(value="/getHash09",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo09(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        //返回的是redis操作的一些参数，使用没有了解明确……
+        RedisOperations operations = hashOperations.getOperations();
 
+        return SystemResult.ok();
+    }
+
+    @RequestMapping(value="/getHash10",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo10(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        /**
+         * 方法putIfAbsent（H k,HK k,HV v）存入单个map,并判断是否存在，
+         * 存在则返回结果为false注意不做任何对redis数据的操作，不存在就添加并返回ture，
+         * 这里只比对row列和key列是否存在一样的，如何value列不一样，同样会判断存在返回false，并不会修改这个value
+         */
+
+        Boolean aBoolean = hashOperations.putIfAbsent("newRedis_cxq3", "user", 0);
+
+        return SystemResult.ok();
+    }
+
+    @RequestMapping(value="/getHash11",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo11(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+
+        //单个的方式写入redis,如何已经存在了相同的hash值和key则会覆盖掉value
+        hashOperations.put("newRedis_4","user",1);
+
+        return SystemResult.ok();
+    }
+
+    @RequestMapping(value="/getHash12",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo12(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        //声明的方式写入redis
+        /**
+         * 如果hash值一样，key也相同会覆盖，如果key不相同会在这个hash值里面继续添加
+         */
+        Map map=new HashMap<>();
+        map.put("k1", "value11");
+        map.put("k2", "value22");
+        map.put("k3", "value33");
+        map.put("k4", "value44");
+        hashOperations.putAll("newRedis_cxq5",map);
+
+        return SystemResult.ok();
+    }
+
+    @RequestMapping(value="/getHash13",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo13(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        //声明的方式写入redis
+        /**
+         * 如果hash值一样，key也相同会覆盖，如果key不相同会在这个hash值里面继续添加
+         */
+        Map map=new HashMap<>();
+        map.put("k1", "value11");
+        map.put("k2", "value22");
+        map.put("k3", "value33");
+        map.put("k4", "value44");
+        hashOperations.putAll("newRedis_cxq5",map);
+
+        return SystemResult.ok();
+    }
+
+    @RequestMapping(value="/getHash14",method = RequestMethod.POST)
+    @ResponseBody
+    public SystemResult demo14(){
+        //获取hash操作对象
+        HashOperations hashOperations = redisTemplate.opsForHash();
+        /**
+         * 匹配获取键值对，ScanOptions.NONE为获取全部键对
+         * ScanOptions.scanOptions().match("map1").build() 匹配获取键位map1的键值对,不能模糊匹配。
+         * 注意：
+         *      这个方法的自动返回不会自动给我们添加上<Map.Entry<Object,Object>>
+         */
+        Cursor<Map.Entry<Object,Object>> scan = hashOperations.scan("newRedis_cxq5", ScanOptions.scanOptions().match("k1").build());
+        //应为实现了迭代器，只能通过迭代来获取keyvalue
+        while (scan.hasNext()) {
+            Map.Entry<Object, Object> entry = scan.next();
+            log.info("通过scan(H key, ScanOptions options)方法获取匹配键值对:" + entry.getKey() + "---->" + entry.getValue());
+        }
+        
         return SystemResult.ok();
     }
 }
--- src/test/java/cn/caixiaobai/springboot/MyConfigRedisTemplateTest.java
+++ src/test/java/cn/caixiaobai/springboot/MyConfigRedisTemplateTest.java
@@ -23,8 +23,8 @@ import java.util.Map;
  * @Date: 2020/11/24 10:28
  * @Version 1.0
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringbootApplication.class)//这个是测试出现问题的解决，需要系写上自己的启动类名称
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = SpringbootApplication.class)//这个是测试出现问题的解决，需要系写上自己的启动类名称
 public class MyConfigRedisTemplateTest {
 
     //注入RedisTemplate
@@ -34,21 +34,21 @@ public class MyConfigRedisTemplateTest {
     /**
      * 操作String
      */
-    @Test
-    public void test1(){
-        //获取操作String'类型的对象
-        ValueOperations valueOperations = redisTemplate.opsForValue();
-        //存储数据
-        try {
-            valueOperations.set("user1","添哥");
-            valueOperations.set("user2","小添哥哥");
-            System.out.println("存储数据成功");
-            //获取数据
-            String user1 = (String)valueOperations.get("user1");
-            String user2 = (String)valueOperations.get("user2");
-            System.out.println(user1 + "==>" + user2);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+    //@Test
+//    public void test1(){
+//        //获取操作String'类型的对象
+//        ValueOperations valueOperations = redisTemplate.opsForValue();
+//        //存储数据
+//        try {
+//            valueOperations.set("user1","添哥");
+//            valueOperations.set("user2","小添哥哥");
+//            System.out.println("存储数据成功");
+//            //获取数据
+//            String user1 = (String)valueOperations.get("user1");
+//            String user2 = (String)valueOperations.get("user2");
+//            System.out.println(user1 + "==>" + user2);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
 }
