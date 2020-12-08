0496263b881ded7a6546083b10178afe0d074d3a
蔡序强 <1781088743@qq.com>
2020-12-08 09:43:28 +0800

    mapper

---
+++ src/main/java/cn/caixiaobai/springboot/mapper/MenuMapper.java
@@ -0,0 +1,16 @@
+package cn.caixiaobai.springboot.mapper;
+
+import cn.caixiaobai.springboot.pojo.Menu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 菜单表 Mapper 接口
+ * </p>
+ *
+ * @author caixiaobai
+ * @since 2020-11-24
+ */
+public interface MenuMapper extends BaseMapper<Menu> {
+
+}
---
+++ src/main/java/cn/caixiaobai/springboot/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package cn.caixiaobai.springboot.mapper;
+
+import cn.caixiaobai.springboot.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author caixiaobai
+ * @since 2020-11-24
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
