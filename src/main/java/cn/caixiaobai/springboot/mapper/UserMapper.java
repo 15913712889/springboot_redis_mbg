5bb32c069405bdf1252235d00a4bab638c4803cf
蔡序强 <1781088743@qq.com>
2020-12-10 11:41:25 +0800

    redis注解使用，未完成

--- src/main/java/cn/caixiaobai/springboot/mapper/UserMapper.java
+++ src/main/java/cn/caixiaobai/springboot/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package cn.caixiaobai.springboot.mapper;
 
 import cn.caixiaobai.springboot.pojo.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * <p>
@@ -11,6 +13,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author caixiaobai
  * @since 2020-11-24
  */
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
+
 
 }
