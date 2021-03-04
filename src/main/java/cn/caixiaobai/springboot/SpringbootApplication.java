7fcf6953658fe4d9d74b36c412f755fe6916aa51
蔡序强 <1781088743@qq.com>
2021-03-04 12:06:51 +0800

    启动项增加内容

--- src/main/java/cn/caixiaobai/springboot/SpringbootApplication.java
+++ src/main/java/cn/caixiaobai/springboot/SpringbootApplication.java
@@ -1,11 +1,18 @@
 package cn.caixiaobai.springboot;
 
 
+import cn.hutool.core.date.DateUtil;
+
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.Date;
 
 /**
  * @Author: caixiaobai
@@ -17,15 +24,40 @@ import org.springframework.cache.annotation.EnableCaching;
 @Slf4j
 @EnableCaching
 public class SpringbootApplication {
+
+    @Value("${server.port}")
+    private static String ServerPort;
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class,args);
-        System.out.println("  .   ____          _            __ _ _\n" +
-                " /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\\n" +
-                "( ( )\\___ | '_ | '_| | '_ \\/ _` | \\ \\ \\ \\\n" +
-                " \\\\/  ___)| |_)| | | | | || (_| |  ) ) ) )\n" +
-                "  '  |____| .__|_| |_|_| |_\\__, | / / / /\n" +
-                " =========|_|==============|___/=/_/_/_/\n" +
-                " :: Spring Boot ::        (v2.3.4.RELEASE)");
-        log.info("恭喜！项目启动成功！");
+        ConfigurableApplicationContext run = SpringApplication.run(SpringbootApplication.class, args);
+        Environment bean = run.getBean(Environment.class);
+        System.out.println(
+                "┴┬┴┬／￣＼＿／￣＼ \n"+
+                "┬┴┬┴▏　　▏▔▔▔▔▔▔▔＼\n"+
+                "┴┬┴／＼　／　　　　　　＼\n"+
+                "┬┴∕　　　　　　　／　　　）\n"+
+                "┴┬▏来看你喽！     　●／▏＼\n"+
+                "┬┴▏　　　　　　　　　　　▔█ \n"+
+                "┴◢██◣　　　　　　／＼＿＿／\n"+
+                "┬█████◣　　　　　　　／　　　\n"+
+                "┴█████████████◣　\n"+
+                "◢██████████████▆▄\n"+
+                "█◤◢██◣◥█████████◤＼　　\n"+
+                "◥◢████　████████◤　　 ＼\n"+
+                "┴█████　██████◤　　　　　 ﹨\n"+
+                "┬│　　　│█████◤　　　　　　　　▏\n"+
+                "┴│　　　│　　　　　　　　　　　　　　▏\n"+
+                "┬∕　　　∕　　　　／▔▔▔＼　　　　 ∕\n"+
+                "*∕＿＿_／﹨　　　∕　　　　　 ＼　　／＼\n"+
+                "┴┬┴┬┴┬┴ ＼＿＿＿＼　　　　 ﹨／▔＼﹨／▔＼\n"+
+                "▲△▲▲╓╥╥╥╥╥╥╥╥＼　　 ∕　 ／▔﹨　／▔\n"+
+                "　＊＊＊╠╬╬╬╬╬╬╬╬＊﹨\n"+
+                " :: Spring Boot ::        (v2.3.4.RELEASE) \n" +
+                "================================================================================= \n"+
+                "================================================================================= \n"+
+                "================================================================================= \n"+
+                "===================================项目启动成功! \n"+
+                "===================================启动端口号："+bean.getProperty("server.port")+"\n"+
+                "===================================启动时间："+DateUtil.date(System.currentTimeMillis())+"\n"+
+                "===================================项目访问链接：http://localhost:" +bean.getProperty("server.port")+bean.getProperty("server.servlet.context-path"));
     }
 }
